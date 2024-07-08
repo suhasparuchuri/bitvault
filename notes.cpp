a8a1c3b6a370e30782a29c03c5bc603cc862c3ab
Sai Suhas Paruchuri <76737982+suhasparuchuri@users.noreply.github.com>
2024-07-08 15:44:20 +0530

    Update notes.cpp

--- notes.cpp
+++ notes.cpp
@@ -1,2 +1,9 @@
 // LCM(A,B) * GCD(A,B) = A*B 
 // Fibonacci(n)=round((((sqrt(5)+1)/2)^n)/sqrt(5))
+
+/*
+  TC
+  Ordered map - always O(logn)
+  Unordered map - Best and Avg - O(1)
+                  Worst - O(n) which is vv rare.  
+*/
