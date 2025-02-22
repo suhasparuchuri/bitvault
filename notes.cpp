85ab86a36348bb275345744db7c39f09df67545c
Sai Suhas Paruchuri <76737982+suhasparuchuri@users.noreply.github.com>
2025-02-22 15:02:39 -0500

    Update notes.cpp

--- notes.cpp
+++ notes.cpp
@@ -7,3 +7,10 @@
   Unordered map - Best and Avg - O(1)
                   Worst - O(n) which is vv rare.  
 */
+
+
+// DP Problem -> Compulsorily need to have 
+//     BOTH 
+//         Overlapping subproblem (F(x) may be calculated multiple times)
+//         Optimal Substructure (Like F(n) depends on F(n-1))
+// Factorial is not an overlapping subproblem coz F(n)=n*F(n-1) there are no values which are calculated multiple times
